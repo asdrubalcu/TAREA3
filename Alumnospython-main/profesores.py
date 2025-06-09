fbf33edb68586f166201c54d523b2774900978e2
asdrubalcu <prof.asdrubal@gmail.com>
2025-06-09 16:53:00 -0600

    Update profesores.py

--- Alumnospython-main/profesores.py
+++ Alumnospython-main/profesores.py
@@ -3,13 +3,11 @@ import cursos
 profesores = []
 
 def agregar_profesor(cedula, nombre, curso):
-    # Validar cédula única
     for p in profesores:
         if p['cedula'] == cedula:
             print("Error: Ya existe un profesor con esa cédula.")
             return
 
-    # Validar existencia del curso
     if not cursos.curso_existe(curso):
         print("Error: El curso no existe.")
         return
